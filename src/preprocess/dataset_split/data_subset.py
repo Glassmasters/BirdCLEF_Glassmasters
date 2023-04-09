7b79c316de3e60a43a105e248460d1ada2deee90
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-04-09 12:01:00 +0200

    edited data split

--- src/preprocess/dataset_split/data_subset.py
+++ src/preprocess/dataset_split/data_subset.py
@@ -3,8 +3,6 @@ import pandas as pd
 from sklearn.model_selection import GroupShuffleSplit
 from clearml import Task, Dataset
 
-# Initialize ClearML Task
-task = Task.init(project_name="BirdCLEF_Glassmasters", task_name="create_train_data_subset")
 
 DATASET_BASE_FILE_PATH = r"D:\kaggle_competition\birdclef-2023"
 TRAIN_SET_FILE_DIR = r"\train_audio"
@@ -12,28 +10,33 @@ TRAIN_SET_FILE_DIR = r"\train_audio"
 # Load the metadata file
 train_metadata = pd.read_csv(DATASET_BASE_FILE_PATH + r"\train_metadata.csv")
 
-# Split the data into a subset and the full training set using GroupShuffleSplit
-gss = GroupShuffleSplit(n_splits=1, test_size=0.9, random_state=42)
-train_idx, full_idx = next(gss.split(train_metadata, groups=train_metadata["primary_label"]))
 
-train_metadata_subset = train_metadata.iloc[train_idx]
-train_metadata_full = train_metadata.iloc[full_idx]
+# Split the data into train, test, and validation sets using GroupShuffleSplit
+gss = GroupShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+train_idx, test_idx = next(gss.split(train_metadata, groups=train_metadata["primary_label"]))
 
-# Save the subset and full training set metadata to CSV files
-train_metadata_subset.to_csv("train_metadata_subset.csv", index=False)
-train_metadata_full.to_csv("train_metadata_full.csv", index=False)
+train_metadata_temp = train_metadata.iloc[train_idx]
+test_metadata = train_metadata.iloc[test_idx]
 
-# Create a ClearML Dataset for the subset and full training set metadata
-subset = Dataset.create(dataset_name='subset', dataset_project='BirdCLEF_Glassmasters')
-subset.add_files('train_metadata_subset.csv')
-subset.upload()
-subset.finalize()
+gss = GroupShuffleSplit(n_splits=5, test_size=0.25, random_state=42)
+train_idx, val_idx = next(gss.split(train_metadata_temp, groups=train_metadata_temp["primary_label"]))
 
-full = Dataset.create(dataset_name='full', dataset_project='BirdCLEF_Glassmasters')
-full.add_files('train_metadata_full.csv')
-full.upload()
-full.finalize()
+train_metadata = train_metadata_temp.iloc[train_idx]
+val_metadata = train_metadata_temp.iloc[val_idx]
 
-# Log the results
-task.get_logger().report_table(title="Subset Metadata", series="Metadata", iteration=0, table_plot=train_metadata_subset.head())
-task.get_logger().report_table(title="Full Metadata", series="Metadata", iteration=0, table_plot=train_metadata_full.head())
+# Create a local subset for testing (25% of the whole dataset)
+gss = GroupShuffleSplit(n_splits=5, test_size=0.25, random_state=42)
+_, local_subset_idx = next(gss.split(train_metadata, groups=train_metadata["primary_label"]))
+local_metadata_subset = train_metadata.iloc[local_subset_idx]
+
+# Save the train, test, and validation sets to CSV files
+train_metadata.to_csv("../../../data/train_metadata.csv", index=False)
+test_metadata.to_csv("../../../data/test_metadata.csv", index=False)
+val_metadata.to_csv("../../../data/val_metadata.csv", index=False)
+local_metadata_subset.to_csv("../../../data/local_subset.csv", index=False)
+
+
+
+
+
+
