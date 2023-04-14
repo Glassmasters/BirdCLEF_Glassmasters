1321a67131dc279083b3cc4efe416809dd39a021
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-04-15 01:21:12 +0200

    set manual seed and progress bar

--- src/audio_to_image_model/model_audio_to_image.py
+++ src/audio_to_image_model/model_audio_to_image.py
@@ -1,16 +1,19 @@
 import torch
 import torch.nn as nn
 import pandas as pd
+from tqdm import tqdm
+import torch.optim as optim
+import warnings
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
 
 from audio_to_image_model_def import CustomCNN
 from audio_to_image_dataset_def import BirdDataset
-from torch.utils.data import random_split
-from torch.utils.data import DataLoader
-import torch.optim as optim
-import warnings
 from src.preprocess.audio_to_image.data_augmentation import AugmentMelSpectrogram
 
 warnings.filterwarnings("ignore")
+
+torch.manual_seed(42)
 
 
 def load_metadata(file_path):
@@ -25,11 +28,13 @@ def init_weights(m):
         m.bias.data.fill_(0.01)
 
 
-def train(dataloader, model, loss_fn, optimizer):
+def train(dataloader, model, loss_fn, optimizer, epoch, total_epochs):
     size = len(dataloader.dataset)
     model.train()
 
-    for batch_idx, (data, target) in enumerate(train_loader):
+    progress_bar = tqdm(train_loader)
+
+    for batch_idx, (data, target) in enumerate(progress_bar):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
@@ -37,33 +42,38 @@ def train(dataloader, model, loss_fn, optimizer):
         loss.backward()
         optimizer.step()
 
-        if batch_idx % 100 == 0:
-            loss, current = loss.item(), batch_idx * len(data)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        loss, current = loss.item(), batch_idx * len(data)
+        progress_bar.set_description(f"Epoch [{epoch}/{total_epochs}]")
+        progress_bar.set_postfix(loss= loss, accuracy= (100 * current / size))
 
-def test(dataloader, model, loss_fn):
+
+def _test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
-    model.eval()
     test_loss, correct = 0, 0
     model.eval()
     total_loss = 0
+
+    progress_bar = tqdm(val_loader)
+
     with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(val_loader):
+        for batch_idx, (data, target) in enumerate(progress_bar):
             data, target = data.to(device), target.to(device)
             output = model(data)
-            loss = criterion(output, target)
+            loss = loss_fn(output, target)
             total_loss += loss.item()
+
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    progress_bar.set_description(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
 
 # Define the dataset base path and the train set file directory
 DATASET_BASE_FILE_PATH = r"D:\kaggle_competition\birdclef-2023"
 TRAIN_SET_FILE_DIR = r"\train_audio"
 
 # Load the metadata file
-metadata_df, num_classes = load_metadata(r"train_metadata_subset.csv")
+metadata_df, num_classes = load_metadata(r"../../data/local_subset.csv")
 
 # Create a custom dataset object
 augmentations = AugmentMelSpectrogram()
@@ -99,16 +109,13 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 # Train the model
 num_epochs = 10
 for epoch in range(num_epochs):
-    print(f"Epoch {epoch + 1}\n-------------------------------")
     # Train
-    train(train_loader, model, criterion, optimizer)
+    train(train_loader, model, criterion, optimizer, epoch +1, num_epochs)
     # Evaluate on the validation set
-    test(val_loader, model, criterion)
+    _test(val_loader, model, criterion)
 
 torch.save(model.state_dict(), "model.pth")
 print("Saved PyTorch Model State to model.pth")
-
-
 
 # TODO: Different optimizer and loss function
 # TODO: Different augmentation techniques
