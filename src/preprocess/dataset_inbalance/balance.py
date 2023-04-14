9cbdea90f7b8cb6a9ea240f805d422d2975fefc8
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-04-14 21:38:11 +0200

    edited balance dataset to use a target_sample number

--- src/preprocess/dataset_inbalance/balance.py
+++ src/preprocess/dataset_inbalance/balance.py
@@ -1,11 +1,9 @@
-# This file defines how to handle the imbalance of the dataset
-
 import pandas as pd
 from sklearn.utils import resample
 from src.plotting.plot_class_occurences import plot_class_occurrences
 
 
-def balance_dataset(metadata_df, sampling_strategy='auto'):
+def balance_dataset(metadata_df, sampling_strategy='auto', target_samples=None):
     """
     Perform resampling on the dataset to handle class imbalance.
 
@@ -14,26 +12,29 @@ def balance_dataset(metadata_df, sampling_strategy='auto'):
                               If a float, it should be in the range (0, 1) and represents the desired ratio of the
                               minority class to the majority class after resampling.
                               Default is 'auto', which uses oversampling for the minority class.
+    :param target_samples: The target number of samples for each class after resampling. If set, it will override
+                           the sampling_strategy parameter.
     :return: A balanced DataFrame
     """
-    if sampling_strategy == 'auto':
-        sampling_strategy = 'oversample'
+    if target_samples is None:
+        if sampling_strategy == 'auto':
+            sampling_strategy = 'oversample'
 
-    grouped = metadata_df.groupby('primary_label')
-    class_counts = grouped.size()
+        grouped = metadata_df.groupby('primary_label')
+        class_counts = grouped.size()
 
-    if sampling_strategy == 'oversample':
-        max_class_count = class_counts.max()
-    elif sampling_strategy == 'undersample':
-        max_class_count = class_counts.min()
-    else:
-        max_class_count = int(class_counts.max() * sampling_strategy)
+        if sampling_strategy == 'oversample':
+            target_samples = class_counts.max()
+        elif sampling_strategy == 'undersample':
+            target_samples = class_counts.min()
+        else:
+            target_samples = int(class_counts.max() * sampling_strategy)
 
     resampled_dfs = []
-    for label, group in grouped:
+    for label, group in metadata_df.groupby('primary_label'):
         resampled_group = resample(group,
-                                   replace=True,
-                                   n_samples=max_class_count,
+                                   replace=len(group) < target_samples,
+                                   n_samples=target_samples,
                                    random_state=42)
         resampled_dfs.append(resampled_group)
 
@@ -47,8 +48,8 @@ if __name__ == '__main__':
     # Load the metadata file
     metadata_df = pd.read_csv(DATASET_BASE_FILE_PATH + r"\train_metadata.csv")
 
-    # Balance the dataset
-    balanced_metadata_df = balance_dataset(metadata_df)
+    # Balance the dataset with a target number of samples per class
+    balanced_metadata_df = balance_dataset(metadata_df, target_samples=100)
 
     # Save the balanced metadata file
     balanced_metadata_df.to_csv(r"../../../data/train_metadata_subset_balanced.csv", index=False)
