b51a08ba6bc7e1b5ca94faf6c40b8c5d17337b3d
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-04-16 14:52:31 +0200

    Submission adapted to the rules of the competition

--- src/audio_to_image_model/audio_to_iamge_standalone_predict.py
+++ src/audio_to_image_model/audio_to_iamge_standalone_predict.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import numpy as np
 import pandas as pd
@@ -18,13 +19,12 @@ def split_audio(audio_path, segment_length=5, sample_rate=32000, n_fft=2048):
         end = start + segment_length
         if end > duration:
             end = duration
-        start_frame = int(start * (sample_rate / (n_fft // 2) ))
-        end_frame = int(end * (sample_rate / (n_fft // 2) ))
+        start_frame = int(start * (sample_rate / (n_fft // 2)))
+        end_frame = int(end * (sample_rate / (n_fft // 2)))
         segment = mel_spectrogram_full[..., start_frame:end_frame]
         segments.append(segment)
 
     return segments
-
 
 
 def predict(model, audio_segments, device):
@@ -44,16 +44,17 @@ def generate_csv(predictions, threshold, row_id_prefix, label_to_bird_map):
 
     for i, pred in enumerate(predictions):
         print(f"Prediction {i}: {pred}")
-        birds_present = [1 if p >= threshold else 0 for p in pred[0]]
-        row_id = f"{row_id_prefix}_{i * 5}"
-        result_rows.append([row_id] + birds_present)
+        birds_present = {label_to_bird_map[index]: 1 if p >= threshold else 0 for index, p in enumerate(pred[0])}
+        row_id = f"{row_id_prefix}_{i * 5 + 5}"
+        result_rows.append([row_id] + list(birds_present.values()))
 
-    result_df = pd.DataFrame(result_rows, columns=['row_id'] + [f'bird{i}' for i in range(1, len(label_to_bird_map) + 1)])
+    # Change the column names to bird names
+    column_names = ['row_id'] + [label_to_bird_map[i] for i in range(0, len(label_to_bird_map))]
+    result_df = pd.DataFrame(result_rows, columns=column_names)
     return result_df
 
 
-
-def inference(audio_path, model, label_to_bird_map, threshold=0.6, device='cpu'):
+def inference(audio_path, model, label_to_bird_map, threshold=0.5017, device='cpu'):
     audio_segments = split_audio(audio_path)
     model.to(device)
     model.eval()
@@ -73,16 +74,31 @@ if __name__ == '__main__':
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # Load your model and preprocessing function here
-    preprocess_fn = preprocess_audio
-    metadata_df, num_classes = load_metadata(r"../../data/local_subset.csv")
+    metadata_df, num_classes = load_metadata(r"D:\kaggle_competition\birdclef-2023\train_metadata.csv")
     model = CustomCNN(num_classes).to(device)
-    model.load_state_dict(torch.load("../../models/model.pth"))
+    model.load_state_dict(torch.load("../../models/model_kaggle.pth", map_location=torch.device(device)))
 
     label_to_index_map = {label: index for index, label in enumerate(metadata_df['primary_label'].unique())}
-    label_to_bird_map = {index: label for label, index in label_to_index_map.items()}  # Assuming label_to_index_map is the dictionary mapping bird labels to indices
+    label_to_bird_map = {index: label for label, index in label_to_index_map.items()}
 
-    audio_path = TEST_FILE_PATH + r"\soundscape_29201.ogg"
-    print(audio_path)
-    result_df = inference(audio_path, model, label_to_bird_map, device=device)
+    # Create an empty DataFrame to store concatenated results
+    submission_df = pd.DataFrame()
 
-    result_df.to_csv('submission.csv', index=False)
+    for audio_path in list(glob.glob(rf"{TEST_FILE_PATH}/*.ogg")):
+        print(audio_path)
+
+        # Get the result DataFrame for the current audio file
+        result_df = inference(audio_path, model, label_to_bird_map, device=device)
+
+        # Concatenate the result_df to the main combined_result_df
+        submission_df = pd.concat([submission_df, result_df], axis=0, ignore_index=True)
+
+    submission_df.to_csv('submission.csv', index=False)
+
+    # Load the submission file into pandas
+    # submission = pd.read_csv('submission.csv')
+    ## count how often the number 1 occurs in each row
+    # submission['count'] = submission.iloc[:, 1:].sum(axis=1)
+    ## select only the rows where the number 1 occurs more than once
+    # submission = submission[submission['count'] > 0]
+    # print(submission.head(100))
